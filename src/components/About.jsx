81721f254e32747e3c616b2ef7b9c654660f0842
coneastdev <coneastdev@proton.me>
2026-02-14 20:10:06 +0000

    fix tailwind in about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,3 +1,5 @@
+import '../styles/global.css'
+
 import SiteMap from './SiteMap';
 
 import Paper from '@mui/material/Paper';
@@ -5,12 +7,12 @@ import Paper from '@mui/material/Paper';
 export default function InsetList() {
   return (
     <div className="flex">
-      <div className="w-32 flex-1">
+      <div className="w-32 flex-1 m-5">
         <Paper elevation={3}>
           <h3>test</h3>
         </Paper>
       </div>
-      <div className="w-64 flex-2">
+      <div className="w-32 flex-1 m-5">
         <Paper elevation={3}>
           <SiteMap />
         </Paper>
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js,jsx,astro}'],
+  theme: {
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+  },
+}
